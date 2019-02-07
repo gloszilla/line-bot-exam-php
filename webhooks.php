d64601acbbb6fdcc37e8f62e8898b16d7dcff82f
gloszilla <30433421+gloszilla@users.noreply.github.com>
2019-02-07 14:54:57 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'igZM0mEyeTvRDA4JyAkRlmAwEcIX70Ad509ilqE54mT6Uasr+nf7R2MfymM9C+ngs/6Jh4npCM57vfbPP67iY2A+T5DZ2S1z7nzFwKTxL+RuqWX9hGywfK66gy2zz+1WP1anxRjumM2E187lIoEXplGUYhWQfeY8sLGRXgo3xvw=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
